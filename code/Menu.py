f894594da68476bda74e0b261c3b842581b92352
Ryan17q <ryanthassio1@gmail.com>
2026-03-15 11:26:49 -0300

    Creating Menu

--- code/Menu.py
+++ code/Menu.py
@@ -1,10 +1,64 @@
 #!/usr/bin/python
 #-* coding: utf-8 -*-
+from cmath import rect
+
+import pygame.image
+from pygame import Surface, Rect
+from pygame.font import Font
+
+from code.CONST import COLOR_BLUE, COLOR_DARKBLUE, MENU_OPTION, COLOR_WHITE, CONTROLS, COLOR_BLACK
+
 
 class Menu:
 
     def __init__(self, window):
         self.window = window
+        self.surf = pygame.image.load("./Assets/Menu_Background.png")
+        self.rect = self.surf.get_rect(left=0, top=0)
 
-    def run(Self, ):
-        pass
+    def run(self, ):
+        pygame.mixer_music.load("./Assets/MenuMusic.mp3")
+        pygame.mixer_music.play(-1)
+        while True:
+            self.window.blit(source=self.surf, dest=self.rect)
+            #Contornos:
+            self.menu_text(70, 'Shinobi', COLOR_DARKBLUE, (380, 67))
+            self.menu_text(70, 'Shinobi', COLOR_DARKBLUE, (385, 67))
+            self.menu_text(70, 'Shinobi', COLOR_DARKBLUE, (390, 67))
+            self.menu_text(70, 'Shinobi', COLOR_DARKBLUE, (395, 67))
+            self.menu_text(70, 'Shinobi', COLOR_DARKBLUE, (415, 67))
+            self.menu_text(70, 'Shinobi', COLOR_DARKBLUE, (420, 67))
+            self.menu_text(70, 'Shinobi', COLOR_DARKBLUE, (425, 67))
+            self.menu_text(70, 'Shinobi', COLOR_DARKBLUE, (395, 67))
+            self.menu_text(70, 'Runner', COLOR_DARKBLUE, (415, 120))
+            self.menu_text(70, 'Runner', COLOR_DARKBLUE, (420, 120))
+            self.menu_text(70, 'Runner', COLOR_DARKBLUE, (425, 120))
+            self.menu_text(70, 'Runner', COLOR_DARKBLUE, (380, 120))
+            self.menu_text(70, 'Runner', COLOR_DARKBLUE, (385, 120))
+            self.menu_text(70, 'Runner', COLOR_DARKBLUE, (390, 120))
+
+            self.menu_text(70, 'Shinobi', COLOR_WHITE, (400, 70))
+            self.menu_text(70, 'Runner', COLOR_WHITE, (400, 118))
+
+            for i in range(len(MENU_OPTION)):
+                pygame.draw.rect(surface=self.surf, color=COLOR_DARKBLUE, rect=(285, 276, 230,50), border_radius=10)
+                pygame.draw.rect(surface=self.surf, color=COLOR_DARKBLUE, rect=(285, 340, 230,50), border_radius=10)
+                self.menu_text(30, MENU_OPTION[i], COLOR_WHITE,(400, 300 + 65 * i))
+            for i in range(len(CONTROLS)):
+                self.menu_text(20, CONTROLS[i], COLOR_DARKBLUE, (698, 35 + 30 * i))
+                self.menu_text(20, CONTROLS[i], COLOR_WHITE, (700, 35 + 30 * i))
+
+            pygame.display.flip()
+
+            # Check for all events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()  # CLose Window
+                    quit()  # end pygame
+
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.Font("fonts/Ninja Attack Italic.ttf", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
+
--- code/game.py
+++ code/game.py
@@ -10,16 +10,11 @@ class Game:
 
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 480))
+        self.window = pygame.display.set_mode(size=(800, 450))
 
     def run(self):
         while True:
             menu = Menu(self.window)
             menu.run()
-            pass
 
-            # Check for all events
-            #for event in pygame.event.get():
-                #if event.type == pygame.QUIT:
-                    #pygame.quit()  # CLose Window
-                    #quit()  # end pygame
+
